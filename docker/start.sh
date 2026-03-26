efe36578ab971433f17d881342696b7c1c30bd2c
Abu Sayeed <itplusplus0@gmail.com>
2026-03-26 15:21:03 -0400

    updated seeder

--- docker/start.sh
+++ docker/start.sh
@@ -45,8 +45,41 @@ php artisan storage:link || true
 echo "Running pending migrations..."
 php artisan migrate --force
 
-echo "Running user seeder..."
-php artisan db:seed --class=Database\\Seeders\\UserSeeder --force
+echo "Running Database seeder... "
+php artisan db:seed --force
+
+# echo "Running user seeder..."
+# php artisan db:seed --class=Database\\Seeders\\UserSeeder --force
+
+# echo "Running brand seeder..."
+# php artisan db:seed --class=Database\\Seeders\\BrandSeeder --force
+
+# echo "Running category seeder..."
+# php artisan db:seed --class=Database\\Seeders\\CategorySeeder --force
+
+# echo "Running customer seeder..."
+# php artisan db:seed --class=Database\\Seeders\\CustomerSeeder --force
+
+# echo "Running coupon seeder..."
+# php artisan db:seed --class=Database\\Seeders\\CouponSeeder --force
+
+# echo "Running database seeder..."
+# php artisan db:seed --class=Database\\Seeders\\DatabaseSeeder --force
+
+# echo "Running environment seeder..."
+# php artisan db:seed --class=Database\\Seeders\\EnvironmentBootstrapperSeeder --force
+
+# echo "Running product seeder..."
+# php artisan db:seed --class=Database\\Seeders\\ProductSeeder --force
+
+# echo "Running random seeder..."
+# php artisan db:seed --class=Database\\Seeders\\RandomSeeder --force
+
+# echo "Running setting seeder..."
+# php artisan db:seed --class=Database\\Seeders\\SettingSeeder --force
+
+# echo "Running Filament"
+# php artisan make:filament-resource Category --generate
 
 echo "Clearing Laravel caches..."
 php artisan optimize:clear || true
