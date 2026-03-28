58ab070dcc3523a82efb6ab28bd14b682a427ad5
Abu Sayeed <itplusplus0@gmail.com>
2026-03-27 21:13:34 -0400

    restore to defult upload

--- docker/start.sh
+++ docker/start.sh
@@ -45,20 +45,20 @@ php artisan storage:link || true
 echo "Running pending migrations..."
 php artisan migrate --force
 
-echo "Generating Filament Shield permissions..."
-php artisan shield:generate --all --panel=admin --option=permissions
+# echo "Generating Filament Shield permissions..."
+# php artisan shield:generate --all --panel=admin --option=permissions
 
 # echo "Running Database seeder... "
 # php artisan db:seed --force
 
-echo "Running user seeder..."
-php artisan db:seed --class=Database\\Seeders\\UserSeeder --force
+# echo "Running user seeder..."
+# php artisan db:seed --class=Database\\Seeders\\UserSeeder --force
 
-echo "Running brand seeder..."
-php artisan db:seed --class=Database\\Seeders\\BrandSeeder --force
+# echo "Running brand seeder..."
+# php artisan db:seed --class=Database\\Seeders\\BrandSeeder --force
 
-echo "Running category seeder..."
-php artisan db:seed --class=Database\\Seeders\\CategorySeeder --force
+# echo "Running category seeder..."
+# php artisan db:seed --class=Database\\Seeders\\CategorySeeder --force
 
 # echo "Running customer seeder..."
 # php artisan db:seed --class=Database\\Seeders\\CustomerSeeder --force
