e9c58be3209575a8fbea2757e12f53f61d62dc93
Abu Sayeed <itplusplus0@gmail.com>
2026-04-09 01:21:16 -0400

    Update CloudCart theme colors

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->brandName('CloudCart')
             ->brandLogo(asset('cloudcart-logo.png'))
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Violet,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
